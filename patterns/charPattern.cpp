70b328098bd9a72f615aa52c6e34043147109eef
yes-im-harsh <harshchauhan.cse279@gmail.com>
2023-03-11 23:45:10 +0530

    Stars

--- patterns/charPattern.cpp
+++ patterns/charPattern.cpp
@@ -38,9 +38,117 @@ using namespace std;
 //     }
 // }
 
-void printPattern(int n) {
-    
+// void printPattern(int n)
+// {
+// //     A
+// //    ABC
+// //   ABCDC
+// //  ABCDEDC
+// // ABCDEFEDC
+//     for (int i = 0; i < n; i++)
+//     {
+//         // Spaces
+//         for (int j = 0; j < n - i - 1; j++)
+//         {
+//             cout << " ";
+//         }
+
+//         // Characters
+//         char ch = 'A';
+//         int breakPoint = (2 * i + 1) / 2;
+//         for (int j = 0; j < 2 * i + 1; j++)
+//         {
+//             cout << ch;
+//             if(j <= breakPoint) ch++;
+//             else ch--;
+//         }
+
+//         // Spaces
+//         for (int j = 0; j < n - i - 1; j++)
+//         {
+//             cout << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+// void printPattern(int n)
+// {
+//     // E
+//     // D E
+//     // C D E
+//     // B C D E
+//     // A B C D E
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (char ch = 'E' - i; ch <= 'E'; ch++)
+//         {
+//             cout << ch << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+void printPattern(int n)
+{
+// **********
+// ****  ****
+// ***    ***
+// **      **
+// *        *
+// *        *
+// **      **
+// ***    ***
+// ****  ****
+// **********
+    int initialSpace = 0;
+    for (int i = 0; i < n ; i++)
+    {
+        // stars
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << "*";
+        }
+
+        // spaces
+        for (int j = 0; j < initialSpace; j++)
+        {
+            cout << " ";
+        }
+
+        //  stars
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << "*";
+        }
+        initialSpace += 2;
+        cout << endl;
+    }
+    initialSpace = 8;
+    for (int i = 1; i <= n; i++)
+    {
+        // stars
+        for (int j = 1; j <=i; j++)
+        {
+            cout << "*";
+        }
+
+        // spaces
+        for (int j = 0; j < initialSpace; j++)
+        {
+            cout << " ";
+        }
+
+        //  stars
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        initialSpace -= 2;
+        cout << endl;
+    }
 }
+
 
 int main()
 {
--- patterns/tempCodeRunnerFile.cpp
+++ patterns/tempCodeRunnerFile.cpp
@@ -1,8 +1,1 @@
-for (int i = 1; i <= 5; i++)
-    {
-        for (int j = 1; j <= i; j++)
-        {
-            cout << j;
-        }
-        cout << endl;
-    }
+ch--;
