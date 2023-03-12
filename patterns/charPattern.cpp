7c17a92968118f5e4fe643a3f403b110cddd4890
yes-im-harsh <harshchauhan.cse279@gmail.com>
2023-03-12 23:40:19 +0530

    Patterns Done

--- patterns/charPattern.cpp
+++ patterns/charPattern.cpp
@@ -89,66 +89,146 @@ using namespace std;
 //     }
 // }
 
+// void printPattern(int n)
+// {
+// // **********
+// // ****  ****
+// // ***    ***
+// // **      **
+// // *        *
+// // *        *
+// // **      **
+// // ***    ***
+// // ****  ****
+// // **********
+//     int initialSpace = 0;
+//     for (int i = 0; i < n ; i++)
+//     {
+//         // stars
+//         for (int j = 1; j <= n - i; j++)
+//         {
+//             cout << "*";
+//         }
+
+//         // spaces
+//         for (int j = 0; j < initialSpace; j++)
+//         {
+//             cout << " ";
+//         }
+
+//         //  stars
+//         for (int j = 1; j <= n - i; j++)
+//         {
+//             cout << "*";
+//         }
+//         initialSpace += 2;
+//         cout << endl;
+//     }
+//     initialSpace = 2*n-2;
+//     for (int i = 1; i <= n; i++)
+//     {
+//         // stars
+//         for (int j = 1; j <=i; j++)
+//         {
+//             cout << "*";
+//         }
+
+//         // spaces
+//         for (int j = 0; j < initialSpace; j++)
+//         {
+//             cout << " ";
+//         }
+
+//         //  stars
+//         for (int j = 1; j <= i; j++)
+//         {
+//             cout << "*";
+//         }
+//         initialSpace -= 2;
+//         cout << endl;
+//     }
+// }
+
+// void printPattern(int n)
+// {
+//     int spaces = 2 * n - 2;
+//     for (int i = 1; i <= 2 * n - 1; i++)
+//     {
+//         int star = i;
+//         if (i > n)
+//             star = 2 * n - i;
+//         // stars
+//         for (int j = 1; j <= star; j++)
+//         {
+//             cout << "*";
+//         }
+//         // spaces
+//         for (int j = 1; j <= spaces; j++)
+//         {
+//             cout << " ";
+//         }
+//         // stars
+//         for (int j = 1; j <= star; j++)
+//         {
+//             cout << "*";
+//         }
+//         cout << endl;
+//         if (i < n)
+//             spaces -= 2;
+//         else
+//             spaces += 2;
+//     }
+// }
+
+// void printPattern(int n)
+// {
+
+//     // ****
+//     // *  *
+//     // *  *
+//     // ****
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             if (i == 0 || i == n - 1 || j == 0 || j == n - 1)
+//             {
+//                 cout << "*";
+//             }
+//             else
+//             {
+//                 cout << " ";
+//             }
+//         }
+//         cout << endl;
+//     }
+// }
+
 void printPattern(int n)
 {
-// **********
-// ****  ****
-// ***    ***
-// **      **
-// *        *
-// *        *
-// **      **
-// ***    ***
-// ****  ****
-// **********
-    int initialSpace = 0;
-    for (int i = 0; i < n ; i++)
+    // 4444444
+    // 4333334
+    // 4322234
+    // 4321234
+    // 4322234
+    // 4333334
+    // 4444444
+
+    for (int i = 0; i < 2 * n - 1; i++)
     {
-        // stars
-        for (int j = 1; j <= n - i; j++)
+        for (int j = 0; j < 2 * n - 1; j++)
         {
-            cout << "*";
+            int top = i;
+            int left = j;
+            int bottom = 2 * n - 2 - i;
+            int right = 2 * n - 2 - j;
+            int val = n - min(min(top, bottom), min(left, right));
+            cout << val;
         }
-
-        // spaces
-        for (int j = 0; j < initialSpace; j++)
-        {
-            cout << " ";
-        }
-
-        //  stars
-        for (int j = 1; j <= n - i; j++)
-        {
-            cout << "*";
-        }
-        initialSpace += 2;
-        cout << endl;
-    }
-    initialSpace = 8;
-    for (int i = 1; i <= n; i++)
-    {
-        // stars
-        for (int j = 1; j <=i; j++)
-        {
-            cout << "*";
-        }
-
-        // spaces
-        for (int j = 0; j < initialSpace; j++)
-        {
-            cout << " ";
-        }
-
-        //  stars
-        for (int j = 1; j <= i; j++)
-        {
-            cout << "*";
-        }
-        initialSpace -= 2;
         cout << endl;
     }
 }
-
 
 int main()
 {
